ecac8984553ad97338c0760bd8cf200f62b5db23
NDAKJin <2842462976@qq.com>
2026-08-02 14:42:42 +0800

    create

---
+++ src/main/java/com/travelagent/travelagent/TravelAgentApplication.java
@@ -0,0 +1,13 @@
+package com.travelagent.travelagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgentApplication.class, args);
+    }
+
+}
