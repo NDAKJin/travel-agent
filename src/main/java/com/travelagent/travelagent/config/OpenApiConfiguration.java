3a584b0533bb340b4e200905573053c0ce1e49c5
NDAKJin <2842462976@qq.com>
2026-08-15 01:24:09 +0800

    补充接口文档身份认证

---
+++ src/main/java/com/travelagent/travelagent/config/OpenApiConfiguration.java
@@ -0,0 +1,15 @@
+package com.travelagent.travelagent.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    OpenAPI travelAgentOpenApi() {
+        return new OpenAPI().info(new Info().title("旅行助手接口文档").version("v1"));
+    }
+}
